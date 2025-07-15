86f5876365f16ea4a8fa258dbbc2fdbcb6bfda33
AlanaHugo <youremail@email.com>
2025-07-15 16:35:33 +1000

    Styling updates for the Article filter

--- mettle/client/src/pages/Articles/Articles.js
+++ mettle/client/src/pages/Articles/Articles.js
@@ -4,29 +4,35 @@ import { fetchArticles } from "../../controllers/Articles/fetchArticlesControlle
 import ArticleCard from "../../components/Articles/articleCards";
 import BackToTop from "../../components/BackToTop/BackToTop";
 import ArticleFilter from "../../components/Articles/ArticleFilter";
-import { sortArticles , filterArticles } from "../../controllers/Articles/filterController";
+import {
+  sortArticles,
+  filterArticles,
+} from "../../controllers/Articles/filterController";
+import "../Articles/Articles.css";
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
 
-const [filters, setFilters] = useState({
-  tags: [],
-  sortBy: "",
-});
+  const [filters, setFilters] = useState({
+    tags: [],
+    sortBy: "",
+  });
 
   useEffect(() => {
-    fetchArticles()
-      .then(setArticles)
-      .catch(console.error);
+    fetchArticles().then(setArticles).catch(console.error);
   }, []);
 
-  const filtered = filterArticles (articles, filters);
-  const sorted = sortArticles (filtered, filters.sortBy);
+  const filtered = filterArticles(articles, filters);
+  const sorted = sortArticles(filtered, filters.sortBy);
 
   return (
-    <div className= "page-layout">
-      <ArticleFilter filters={filters} onChange={setFilters} />
-      <Grid container spacing={2} padding={2}>
+    <div className="page-layout">
+      <ArticleFilter
+        className="filterSidebar"
+        filters={filters}
+        onChange={setFilters}
+      />
+      <Grid className="articleGrid" container spacing={2} padding={2}>
         {sorted.map((article) => (
           <Grid item xs={12} sm={6} md={4} key={article._id}>
             <ArticleCard article={article} />
